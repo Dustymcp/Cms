a52d4fa68e7e95cae0e29eb5f97b0e31cc461c87
Dustymcp <jakob@danmarkvip.dk>
2015-04-20 16:32:15 +0200

    Added opneing hours
    opening hours aded to frontend

---
+++ OpeningHours.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class OpeningHours : System.Web.UI.Page
+{
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        var openhoursRepo = new HoursModel.Repository();
+        var openhours = openhoursRepo.Read().First();
+        litOpeningHoursMonday.Text = openhours.Monday;
+        litOpeningHoursTuesday.Text = openhours.Tuesday;
+        litOpeningHoursWednesday.Text = openhours.Wednesday;
+        litOpeningHoursThursday.Text = openhours.Thursday;
+        litOpeningHoursFriday.Text = openhours.Friday;
+        litOpeningHoursSaturday.Text = openhours.Saturday;
+        litOpeningHoursSunday.Text = openhours.Sunday;
+        litComment.Text = openhours.Comment;
+    }
+
+ 
+}
